5fba3e07357115848a3753cf9f5ecf23bd4eb464
Saira <saira.khadka@apexcollege.edu.np>
2023-07-05 10:19:19 +0545

    first commit

---
+++ app/Http/Controllers/ArtSentimentAnalysisController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google\Cloud\Core\ServiceBuilder;
+
+class ArtSentimentAnalysisController extends Controller
+{
+    public function sentiment()
+    {
+        // Code will be added here
+    }
+}
---
+++ app/Http/Controllers/Middleware/Requests/Auth/LoginRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Middleware\Requests\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginRequest extends Controller
+{
+    //
+}
